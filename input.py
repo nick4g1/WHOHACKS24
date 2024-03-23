5939d5b93898189efc4f92f3f7d0234a6c0335dd
Roscoe Lin <roscoe.lin7@gmail.com>
2024-03-23 18:24:44 -0400

    Parsing img

--- input.py
+++ input.py
@@ -1,9 +1,22 @@
+import re
+
 def readInto(name):
+    data = []
     stuff = open(name, "r", encoding="utf-8")
     reading = stuff.read()
+    x = bool(re.search(",", reading))
+    if (x):
+        y = re.split("\n", reading)
+        part = [item.partition(', ') for item in y]
+        part.pop(len(part) - 1)
+        Dict = {}
+        for thing in part:
+            Dict[int(thing[0])] = int(thing[2], 16)
+        data = Dict
+    else:
+        data = [int(item) for item in reading.strip().split('\n')]
+        for thing in data:
+            print(thing)
     stuff.close()
-    data = [int(item) for item in reading.strip().split('\n')]
-    for thing in data:
-        print(thing)
     return data
     
