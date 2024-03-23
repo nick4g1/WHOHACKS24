5a1c6f6e599a490861e874c2293fd822b490f09c
Thomas Nguyen <godcastertam919@gmail.com>
2024-03-23 19:53:09 -0400

    updated image

--- image.py
+++ image.py
@@ -1,33 +1,28 @@
 from PIL import Image
 import numpy as np
 
+def rgb_to_hex(rgb):
+    return '0x{0:02x}{1:02x}{2:02x}'.format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
 img = Image.open('cabbage.jpg')
 
 arr = np.array(img)
-
-
 
 scale_factor = 0.01
 new_width = int(arr.shape[1] * scale_factor)
 new_height = int(arr.shape[0] * scale_factor)
 arr = np.array(Image.fromarray(arr).resize((new_width, new_height)))
 
-print(new_width)
-print(new_height)
-
-
-def rgb_to_hex(rgb):
-    return '0x{0:02x}{1:02x}{2:02x}'.format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
 
 reshaped_array = arr.reshape(-1, arr.shape[-1])
 with open('testImage.txt', 'w') as f:
+    #f.write(str(new_height) + ', ')
+    #f.write(str(new_width) + '\n')
     for i in range(len(reshaped_array)):
         right = np.dot(reshaped_array[i], [0.2989, 0.5870, 0.1140])
         f.write(str(round(right)))
         f.write(", ")
-        f.write(str(rgb_to_hex(reshaped_array[i])))
-        f.write('\n')
-
+        f.write(str(rgb_to_hex(reshaped_array[i])) + '\n')
 
 #with open('testImage.txt', 'w') as f:
 #    for row in reshaped_array:
