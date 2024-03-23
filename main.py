466a50ea5ab3fbd612c7b9fb75e474f7ee9d5153
nick4g1 <nick4g1@vt.edu>
2024-03-23 13:47:24 -0400

    rectangles

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import tkinter as tk
 import sys
 from input import *
+from tkinter import *
 
 
 #File IO
-#readInto(sys.argv[1])
+#x = readInto(sys.argv[1])
 
-
+x = [10,20,30,40,50]
 #thomas was here
 window = tk.Tk()
+canvas = Canvas(window, width=500,height=500)
+canvas.pack()
+def VisualizeArray(x):
+    count = 0;
+    for element in x:
+        rect = canvas.create_rectangle(0 + (20*count),100 + element,20+(20*count),100, fill='black')
+        count = count + 1
 window.geometry("1000x600")
 greeting = tk.Label(text="Algorithm Visualizer")
 greeting.pack()
+VisualizeArray(x)
 window.mainloop()
+
 
