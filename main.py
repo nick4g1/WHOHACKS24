8cd9e25cd8156e93cdec00cafeff4e0ca2dcc787
Roscoe Lin <roscoe.lin7@gmail.com>
2024-03-23 12:56:43 -0400

    IO

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import tkinter as tk
+import sys
+
+#File IO
+input = open(sys.argv[1], "r", encoding="utf-8").read()
+
 
 #thomas was here
 window = tk.Tk()
 greeting = tk.Label(text="Hello, Tkinter")
 greeting.pack()
 window.mainloop()
+
