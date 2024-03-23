246263a7f7a1f8840917f9c7cb3dac31faec9a0a
Roscoe Lin <roscoe.lin7@gmail.com>
2024-03-23 13:56:38 -0400

    changed rec

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from tkinter import *
 
 
 #File IO
-#x = readInto(sys.argv[1])
+x = readInto(sys.argv[1])
 
-x = [10,20,30,40,50]
+#x = [10,20,30,40,50]
 #thomas was here
 window = tk.Tk()
 canvas = Canvas(window, width=500,height=500)
@@ -15,7 +15,7 @@ canvas.pack()
 def VisualizeArray(x):
     count = 0;
     for element in x:
-        rect = canvas.create_rectangle(0 + (20*count),100 + element,20+(20*count),100, fill='black')
+        rect = canvas.create_rectangle(0 + (20*count),100 - element,20+(20*count),100, fill='black')
         count = count + 1
 window.geometry("1000x600")
 greeting = tk.Label(text="Algorithm Visualizer")
