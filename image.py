42b17987d76c7a2a9e0f2e462d1dcf2c52e27ba1
Thomas Nguyen <godcastertam919@gmail.com>
2024-03-23 16:46:00 -0400

    updated image.py

--- image.py
+++ image.py
@@ -1,18 +1,16 @@
-import image
+import PIL.Image
 import numpy as np
 
-img = Image.open('cabbage.jpg')
+img = PIL.Image.open('cabbage.jpg')
 
-arr = np.asarray(img)
-print(arr.shape)
-
-numbers = []
-for row in arr:
-    temp = []
-    for col in row:
-        temp.append(str(col))
-    numbers.append(temp)
+arr = np.array(img)
+scale_factor = 0.02
+new_width = int(arr.shape[1] * scale_factor)
+new_height = int(arr.shape[0] * scale_factor)
+arr = np.array(PIL.Image.fromarray(arr).resize((new_width, new_height)))
+reshaped_array = arr.reshape(-1, arr.shape[-1])
 
 with open('testImage.txt', 'w') as f:
-    for row in numbers:
-        f.write(','.join(row) + '\n')
+    for row in reshaped_array:
+        f.write(str(row[0]))
+        f.write('\n')
