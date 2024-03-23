ead1f92e88c1fb3240782437f1f69be7ad850d75
Thomas Nguyen <godcastertam919@gmail.com>
2024-03-23 17:36:03 -0400

    changed image.py to luminance and hex values

--- image.py
+++ image.py
@@ -4,13 +4,30 @@ import numpy as np
 img = PIL.Image.open('cabbage.jpg')
 
 arr = np.array(img)
-scale_factor = 0.02
+
+
+
+scale_factor = 0.01
 new_width = int(arr.shape[1] * scale_factor)
 new_height = int(arr.shape[0] * scale_factor)
 arr = np.array(PIL.Image.fromarray(arr).resize((new_width, new_height)))
-reshaped_array = arr.reshape(-1, arr.shape[-1])
 
+
+
+def rgb_to_hex(rgb):
+    return '{0:02x}{1:02x}{2:02x}'.format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
+reshaped_array = arr.reshape(-1, arr.shape[-1])
 with open('testImage.txt', 'w') as f:
-    for row in reshaped_array:
-        f.write(str(row[0]))
+    for i in range(len(reshaped_array)):
+        right = np.dot(reshaped_array[i], [0.2989, 0.5870, 0.1140])
+        f.write(str(round(right)))
+        f.write(", ")
+        f.write(str(rgb_to_hex(reshaped_array[i])))
         f.write('\n')
+
+
+#with open('testImage.txt', 'w') as f:
+#    for row in reshaped_array:
+#        f.write(str(row[0]))
+#        f.write('\n')
