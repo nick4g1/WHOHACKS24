efc0ccf2e86a9eb61873192f4b0fb9d8c77aeccc
Thomas Nguyen <godcastertam919@gmail.com>
2024-03-23 20:31:43 -0400

    changed image.py

--- image.py
+++ image.py
@@ -4,25 +4,27 @@ import numpy as np
 def rgb_to_hex(rgb):
     return '0x{0:02x}{1:02x}{2:02x}'.format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
 
-img = Image.open('cabbage.jpg')
+#img = Image.open("cabbage.jpg")
 
-arr = np.array(img)
+def convertImage(im, scale):
+    img = Image.open(im)
+    arr = np.array(img)
 
-scale_factor = 0.01
-new_width = int(arr.shape[1] * scale_factor)
-new_height = int(arr.shape[0] * scale_factor)
-arr = np.array(Image.fromarray(arr).resize((new_width, new_height)))
+    scale_factor = scale
+    new_width = int(arr.shape[1] * scale_factor)
+    new_height = int(arr.shape[0] * scale_factor)
+    arr = np.array(Image.fromarray(arr).resize((new_width, new_height)))
 
 
-reshaped_array = arr.reshape(-1, arr.shape[-1])
-with open('testImage.txt', 'w') as f:
-    #f.write(str(new_height) + ', ')
-    #f.write(str(new_width) + '\n')
-    for i in range(len(reshaped_array)):
-        right = np.dot(reshaped_array[i], [0.2989, 0.5870, 0.1140])
-        f.write(str(round(right)))
-        f.write(", ")
-        f.write(str(rgb_to_hex(reshaped_array[i])) + '\n')
+    reshaped_array = arr.reshape(-1, arr.shape[-1])
+    with open('testImage.txt', 'w') as f:
+        #f.write(str(new_height) + ', ')
+        #f.write(str(new_width) + '\n')
+        for i in range(len(reshaped_array)):
+            right = np.dot(reshaped_array[i], [0.2989, 0.5870, 0.1140])
+            f.write(str(round(right)))
+            f.write(", ")
+            f.write(str(rgb_to_hex(reshaped_array[i])) + '\n')
 
 #with open('testImage.txt', 'w') as f:
 #    for row in reshaped_array:
